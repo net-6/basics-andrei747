e9d95af0d236d75ddbc549e5c7827c86efcb5623
Andrei Botezatu <andrei_botezatu_90@yahoo.com>
2020-01-02 15:22:44 +0200

    Home3

--- recap/Home3/Home3/Program.cs
+++ recap/Home3/Home3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Home3
 {
@@ -12,15 +14,30 @@ namespace Home3
             // recap1.Problem1();
             //var recap2 = new Recap();
             // recap2.Problem2();           
-           // var recap3 = new Recap();
-           // recap3.Problem3();
-            //var recap4 = new Recap();
-           // recap4.Problem4();
-            var recap5 = new Recap();
-            recap5.Problem5();
-
-
-
+            // var recap3 = new Recap();
+            // recap3.Problem3();
+            // var recap4 = new Recap();
+            //recap4.Problem4();
+            // var recap5 = new RecapProblem05();
+            // recap5.Problem5();
+            //var recap6 = new RecapProblem06();
+            // recap6.Problem06();
+           // var recap7 = new RecapProblem07();
+            //recap7.Problem07();
+           // var recap8 = new RecapProblem08();
+           // recap8.Problem08();
+           // var recap9 = new RecapProblem09();
+           // recap9.Problem09();
+           // var recap10 = new RecapProblem10();
+           // recap10.Problem10();
+           // var recap11 = new RecapProblem11();
+           // recap11.Problem11();
+           // var recap12 = new RecapProblem12();
+           // recap12.Problem12();
+            //var recap13 = new RecapProblem13();
+           // recap13.Problem13();
+            var recap14 = new RecapProblem14();
+            recap14.Problem14();
         }
     }
 }
--- recap/Home3/Home3/Recap.cs
+++ recap/Home3/Home3/Recap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
+
 namespace Home3
 {
     enum Image
@@ -82,19 +83,23 @@ namespace Home3
             int speedLimit = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the speed of car");
             int speedofCar = int.Parse(Console.ReadLine());
-            int penaltypoints = 0;
-            int i = 0;
+            int penaltypoints = 0;           
             if (speedofCar < speedLimit)
             {
                 Console.WriteLine("Ok");
             }
             else
             {
-               for(int speedofCar1 = speedLimit; speedofCar1 < speedofCar; speedofCar1 = speedofCar + 5)
+                
+               for( int speedofCar1 = speedLimit; speedofCar1 < speedofCar; speedofCar1 = speedofCar1 + 5)
                     penaltypoints++;
                 {
-                    
-                    Console.WriteLine("You have:"+ penaltypoints+ "penalty points");///in progress
+
+                    Console.WriteLine("You have:" + penaltypoints + " penalty points");
+                    if(penaltypoints > 12)
+                    {
+                        Console.WriteLine("License Suspended");
+                    }
                 }
                                                                                                            
             }
@@ -110,25 +115,365 @@ namespace Home3
         /// Write a program and continuously ask the user to enter different names, until the user presses Enter 
         /// (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
         /// </summary>
+    }
+
+    public class RecapProblem05
+    {
         public void Problem5()
         {
-            Console.WriteLine("Enter a name");
-            string s1 = Console.ReadLine() +  Console.ReadLine() + Console.ReadLine();
-            char [] arr = s1.ToCharArray();
-            Console.WriteLine("Enter a name");
-            // string s2 = Console.ReadLine() + Console.ReadLine() + Console.ReadLine();
-           //in progress
+            var names = new List<string>();
+
+            while (true)
+            {
+                var name = TypeNames();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    names.Add(name);
+                    Console.WriteLine(GetLikes(names));
+
+                }
+
+            }
+
         }
-        //in progress for the other 20 problems
 
+        private static string TypeNames()//create a method to type names
+        {
+            Console.WriteLine("Type a name");
+            return Console.ReadLine();
+        }
+        private static string GetLikes(List<string> names)
+        {
 
+            if (names.Count > 2)
+            {
+                return names[0] + ", " + names[1] + " and " + Reducelist(names).Count + " others like your post";
+            }
+            if (names.Count == 2)
+            {
+                return names[0] + " and " + names[1] + " liked your post";
+            }
+            return names[0] + " liked your post";
+        }
+        private static List<string> Reducelist(List<string> names)
+        {
+            return names.GetRange(2, names.Count - 2);
+        }
 
+    }
+    /// <summary>
+    /// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
+    /// Display the reversed name on the console.
+    /// </summary>
+    public class RecapProblem06
+    {
+        public void Problem06()
+        {
+            Console.WriteLine("Please type your name");
+            string s1 = Console.ReadLine();
+            Array arr = s1.ToArray();
+            Array.Reverse(arr);
+            string s2 = arr.ToString();
+            Console.WriteLine(s2);// i give up...i realy don't kow
+            ///System.Char[]                      
+        }
 
+    }
+    /// <summary>
+    /// Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display 
+    /// an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them 
+    /// and display the result on the console.
+    /// </summary>
+    public class RecapProblem07
+    {
+        public void Problem07()
+        {
+            var n1 = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                while (true)
+                {
+                    int numbers = 0;
+                    numbers = EnterNumbers();
+                    var currentNumber = Array.IndexOf(n1, numbers);
+                    if (currentNumber == -1)
+                    {
+                        n1[i] = numbers;
+                        break;
+                    }
+                    Console.WriteLine("Stop! Please enter a different number");
+                }
+            }
+            Array.Sort(n1);
+            foreach (var num in n1)
+            {
+                Console.Write(num);
+            }
+
+        }
+
+        private static int EnterNumbers()
+        {
+            Console.WriteLine("Enter 5 numbers");
+            return int.Parse(Console.ReadLine());
+
+        }
+
+    }
+    /// <summary>
+    /// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may 
+    /// include duplicates. Display the unique numbers that the user has entered.
+    /// </summary>
+    public class RecapProblem08
+    {
+        public void Problem08()
+        {
+            var numberList = new List<int>();
+            do//execute this first while is true
+            {
+                int rawNumber;
+                Console.Write("Please enter the numbers: ");
+                string input = Console.ReadLine();
+                bool check = int.TryParse(input, out rawNumber);
+                if (check == false && input == "quit")
+                {
+
+                    Console.WriteLine("List unique numbers:");
+                    List<int> distinctList = numberList.Distinct().ToList();//separates the distinct numbers
+                    foreach (var element in distinctList)
+                        Console.WriteLine(element);
+                    break;
+                }
+
+                if (check == false)//if you type letters
+                    Console.WriteLine("To exit type quit or enter more numbers");
+
+                else
+                    numberList.Add(rawNumber);//add the numbers you type in a list
+
+            } while (true);//
+
+        }
 
 
 
     }
 
+    /// <summary>
+    /// Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is 
+    /// empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display 
+    /// the 3 smallest numbers in the list.
+    /// </summary>
+    /* public class RecapProblem09
+     {
+         public void Problem09()
+         {
+             var numbers = new List<int>();
+             int rawNumber;
+                 Console.WriteLine("Enter a list of numbers separated by commas");
+                 var input = Console.ReadLine();
+             string [] s1 =  input.Split(',');
+             bool check = int.TryParse(input, out rawNumber);
+             if(check == true)
+             {
+                 List<int> n1 = numbers.ToList();
+                 n1.Add(s1);//nu stiu
+
+             }
+
+             if(string.IsNullOrEmpty(input) || n1.Count < 5)
+             {
+                 Console.WriteLine("Invalid");
+             }
+
+
+         }
+
+
+     }*/
+    //## Loops
+    /// <summary>
+    /// Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
+    /// Display the result on the console.
+    /// </summary>
+    public class RecapProblem10
+    {
+        public void Problem10()
+        {
+            Console.WriteLine("Numbers divisible by 3: ");
+            int number = 100;
+            int div = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    div = div + 1;
+
+                }
+
+            }
+            Console.WriteLine(div);
+
+        }
+    }
+
+    /// <summary>
+    /// Write a program and continuously ask the user to enter a number. The loop terminates when the user 
+    /// enters “ok". Calculate the sum of all the previously entered numbers and display it on the console.
+    /// </summary>
+    public class RecapProblem11
+    {
+        public void Problem11()
+        {
+            var numberList = new List<int>();
+
+            do
+            {
+
+                string num = Console.ReadLine();
+                int random = 0;
+                if (num == "ok")
+                {
+                    numberList.Add(random);
+                    List<int> number = numberList.ToList();
+                    char[] ch = num.ToCharArray();
+
+
+                    for (int i = 0; i < num.Length; i++)
+                    {
+                        random = ch[i] + ch[i];
+                        Console.WriteLine(random);// stuck
+                    }
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Type numbers");
+                }
+
+
+
+            } while (true);
+
+        }
+
+
+    }
+    /// <summary>
+    /// Write a program which takes a single number from the user,
+    /// calculates the factorial and prints the value on the console.
+    /// For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+    /// and display it as 5! = 120(result of 5 x 4 x 3 x 2 x 1).
+    /// </summary>
+    public class RecapProblem12
+    {
+        public void Problem12()
+        {
+            Console.WriteLine("Type a single number");
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial = factorial * i;
+
+            }
+            Console.WriteLine("{0}! = {1}", number, factorial);
+
+        }
+
+    }
+    /// <summary>
+    /// Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
+    /// If the user guesses the number, display “You won". Otherwise, display “You lost".
+    /// </summary>
+    public class RecapProblem13
+    {
+        public void Problem13()
+        {
+
+
+
+            for (int i = 0; i < 4; i++)
+            {
+
+                int random = int.Parse(Console.ReadLine());
+
+                Random rr = new Random();
+                int guess = rr.Next(1, 10);
+                if (random > guess)
+                {
+
+                    Console.WriteLine("You lost");
+                }
+                else if (random < guess)
+                {
+                    Console.WriteLine("You lost");
+                }
+                else
+                {
+                    Console.WriteLine("You win");
+                }
+
+            }
+
+        }
+
+    }
+    /// <summary>
+    /// Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
+    /// numbers and display it on the result. For example, if the user enters “5, 3, 8, 1, 4", the program should 
+    /// display 8 on the console.
+    /// </summary>
+    public class RecapProblem14
+    {
+        public void Problem14()
+        {
+            
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("Type a series of numbers");
+                int num = int.Parse(Console.ReadLine());
+                int[] arr = { num };
+                Console.WriteLine("The max is: " + arr.Max());//stuck
+
+            }
+            
+
+        }
+
+    }
+////## Strings
+    /// <summary>
+    /// Write a program and ask the user to enter a few numbers separated by a hyphen(minus). Work out 
+    /// if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+    /// display a message: "Consecutive"; otherwise, display "Not Consecutive".
+    /// </summary>
+   
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
